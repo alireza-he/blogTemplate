5789c07bbc0cd4deebbcc8dd3c0521401579e2f3
alireza.hey <heidari.alirezad@gmail.com>
2022-07-12 04:04:52 -0700

    create salam component

---
+++ components/salam.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Salam = () => {
+    return (
+        <div>
+            <h1>
+                This is Salam page
+            </h1>
+        </div>
+    );
+};
+
+export default Salam;
